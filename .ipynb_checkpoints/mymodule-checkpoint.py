b826b7c69f8d6e31c32ecf5bf38aa7568efc02fb
prateekrajgautam <prateekrajgautam@gmail.com>
2024-09-09 10:24:59 +0530

    functions listcomp, lambdas map filter imports

---
+++ .ipynb_checkpoints/mymodule-checkpoint.py
@@ -0,0 +1,19 @@
+PI = 3.14
+
+def myadd(a,b):
+    return a+b
+
+
+def mydiff(a,b):
+    return a-b
+
+
+if __name__ == "__main__":
+    print("executed as main file")
+    a = 1
+    b = 2
+    print(f"addition of { a } and { b } is {myadd(a,b)}")
+else:
+    print("imported as module")
+
+    
